047ccdceaf4cd75ec810788b954b4621cf39ce07
DAVINCI\notok <harshitjawla123@gmail.com>
2023-03-28 21:56:12 +0530

    Echo udp & mod

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-use std::{io, net::Ipv4Addr};
-use tokio::net::UdpSocket;
+use tokio::net::{UdpSocket};
+use tokio::io;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let local_addr = (Ipv4Addr::UNSPECIFIED, 8051);
-    let sock = UdpSocket::bind(local_addr).await?;
-    println!("{:?}", sock.local_addr());
+
+    let local_addr = ("0.0.0.0", 8051);
+
+    let usock = UdpSocket::bind(local_addr).await?;
+    println!("UDP Listening on {:?}", usock.local_addr());
 
     loop {
         let mut buf = [0; 1];
 
-        if let Ok((len, addr)) = sock.recv_from(&mut buf).await {
+        if let Ok((len, addr)) = usock.recv_from(&mut buf).await {
             if &buf[..len] == b"C" {
-                println!("Got '{:?}' request from {:?}", &buf[..len], addr);
-                let len = sock.send_to(&buf[..len], addr).await?;
-                println!("Sent {:?} byte back", len);
+                println!("Got '{:?}' from {:?}", &buf[..len], addr);
+                let len = usock.send_to(&buf[..len], addr).await?;
+                println!("{:?} bytes sent", len);
             }
         }
+        
     }
 }
